e3b4ab7466158580ff06bff2dc6615ff841e393e
shubhampopli <46472934+shubhampopli@users.noreply.github.com>
2024-08-27 23:14:26 -0300

    Delete WEB_DEVLOPMENT_UDEMY_MISC directory

--- WEB_DEVLOPMENT_UDEMY_MISC/backendudemy/intotonpm/FakerApp/app.js
+++
@@ -1,6 +0,0 @@
-var faker = require('faker');
-var i;  
-for (i=0;i<10;i++){
-    
-        console.log(""+i+"  "+faker.commerce.productName()+" -$"+faker.commerce.price());
-}
--- WEB_DEVLOPMENT_UDEMY_MISC/exe.js
+++
@@ -1,5 +0,0 @@
-$("div").css("background","purple")
-$(".highlight").css("width","200px")
-$("#third").css("border","2px solid red")
-$("div:first").css("border","2px solid green")
-
